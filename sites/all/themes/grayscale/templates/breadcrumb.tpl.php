8eeb51d394644511f367808c3b5108ffa609ac74
Denis Krivostchokov <densomart@gmail.com>
2012-06-14 19:07:37 +0700

    removed some files

--- sites/all/themes/grayscale/templates/breadcrumb.tpl.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo "ZZ"; 
-echo $breadcrumb_delimiter;
-?>
-
-<?php if (!empty($breadcrumb)): ?>
-<span class="breadcrumb"><?php print implode(''.$breadcrumb_delimeter.'', $breadcrumb) ?></span>
-<?php endif; ?>
-
